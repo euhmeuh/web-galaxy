39ac8ba5b79bdc7ae9ae67d2091105aebaa785b0
euhmeuh <jerome.martin.dev@gmail.com>
2019-08-08 14:29:59 +0200

    provide a way to easily add new headers to a response

--- web-galaxy-lib/web-galaxy/response.rkt
+++ web-galaxy-lib/web-galaxy/response.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (provide
+  current-response-headers
   current-error-responder
   current-not-found-responder
   req
@@ -11,26 +12,44 @@
   redirect/see-other
   redirect/cookie
   response/page
+  response/raw
   response/file
   response/json
   response/not-found
   response/full
   response/output
   response/xexpr
-  define-response)
+  define-response
+  response-parameterize)
 
 (require
-  (for-syntax racket/base
-              racket/syntax
-              syntax/parse)
+  (for-syntax
+    racket/base
+    racket/syntax
+    syntax/parse)
   racket/match
   racket/port
   racket/stxparam
+  syntax/parse/define
   json
   web-server/http/cookie
   web-server/http/cookie-parse
   web-server/servlet
   "translate.rkt")
+
+(define current-response-headers (make-parameter '()))
+
+(define-simple-macro (response-parameterize ([h-name:id h-value:expr] ...) body:expr ...+)
+  (parameterize ([current-response-headers
+                  (append (current-response-headers)
+                          (list (list 'h-name h-value) ...))])
+    body ...))
+
+(define (response-headers->headers-list headers)
+  (map (lambda (a-header)
+         (header (string->bytes/utf-8 (symbol->string (car a-header)))
+                 (string->bytes/utf-8 (cadr a-header))))
+       headers))
 
 (define-syntax-parameter req
   (lambda (stx)
@@ -41,16 +60,26 @@
     #:preamble #"<!DOCTYPE html>"
     content))
 
-(define (response/file file)
+(define (response/raw #:code [code 200]
+                      #:message [message #"OK"]
+                      #:mimetype [mimetype TEXT/HTML-MIME-TYPE]
+                      bytes)
   (response/full
-    200 #"OK" (current-seconds) TEXT/HTML-MIME-TYPE
-    '()
+    code message (current-seconds) mimetype
+    (response-headers->headers-list (current-response-headers))
+    (list bytes)))
+
+(define (response/file #:mimetype [mimetype TEXT/HTML-MIME-TYPE]
+                       file)
+  (response/full
+    200 #"OK" (current-seconds) mimetype
+    (response-headers->headers-list (current-response-headers))
     (port->bytes-lines (open-input-file file) #:close? #t)))
 
 (define (response/json content)
   (response/full
     200 #"OK" (current-seconds) #"application/json; charset=utf-8"
-    '()
+    (response-headers->headers-list (current-response-headers))
     (list (jsexpr->bytes content))))
 
 (define (response/not-found content)
@@ -71,8 +100,8 @@
 
 (define-response (not-found)
   (response/full
-    404 #"Not Found"
-    (current-seconds) TEXT/HTML-MIME-TYPE '()
+    404 #"Not Found" (current-seconds) TEXT/HTML-MIME-TYPE
+    (response-headers->headers-list (current-response-headers))
     '(#"404 - Not found")))
 
 (define (response-error url exception)
@@ -80,8 +109,8 @@
                     (uri ,(url->string url))
                     (time ,(current-seconds))))
   (response/full
-    500 #"Internal Server Error"
-    (current-seconds) TEXT/HTML-MIME-TYPE '()
+    500 #"Internal Server Error" (current-seconds) TEXT/HTML-MIME-TYPE
+    (response-headers->headers-list (current-response-headers))
     '(#"500 - Internal server error")))
 
 (define current-not-found-responder (make-parameter response-not-found))
